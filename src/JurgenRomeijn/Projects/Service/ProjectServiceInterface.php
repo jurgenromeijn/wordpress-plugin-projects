43b5f2c1607689668eff7a26a4dabb5cf3e29764
Jurgen Romeijn <jurgen.romeijn@incentro.com>
2016-06-24 16:33:12 +0200

    Create the project post type through the ProjectService.

--- src/JurgenRomeijn/Projects/Service/ProjectServiceInterface.php
+++ src/JurgenRomeijn/Projects/Service/ProjectServiceInterface.php
@@ -9,7 +9,7 @@ namespace JurgenRomeijn\Projects\Service;
  * This interface declares all functionality the Projects Portfolio should be able to do.
  * @package JurgenRomeijn\Projects
  */
-interface ProjectsServiceInterface
+interface ProjectServiceInterface
 {
 
     public function createPostType();
