6177678c5f80785c9de62704e90677788243784b
Jurgen Romeijn <jurgen.romeijn@incentro.com>
2016-06-24 23:38:34 +0200

    Added documentation for Model namespace.

--- src/Model/WordpressModelInterface.php
+++ src/Model/WordpressModelInterface.php
@@ -5,9 +5,17 @@
 
 namespace JurgenRomeijn\Projects\Model;
 
+/**
+ * This interface contains methods specifically used for a Wordpress model.
+ * @package JurgenRomeijn\Projects\Model
+ */
 interface WordpressModelInterface
 {
 
+    /**
+     * Get a represnetation of the model in array format.
+     * @return array
+     */
     public function toArray();
 
 }
